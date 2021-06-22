d0c576f35f3efa12840599d55ee943cdc4d860fd
Shelly Jurbinsky <shellyjur@gmail.com>
2021-06-22 23:23:37 +0300

    Created Game buttons

--- GameUI/Board.Designer.cs
+++ GameUI/Board.Designer.cs
@@ -30,35 +30,47 @@ namespace GameUI
         {
             this.player1L = new System.Windows.Forms.Label();
             this.player2L = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // player1L
             // 
             this.player1L.AutoSize = true;
-            this.player1L.Location = new System.Drawing.Point(164, 600);
+            this.player1L.Location = new System.Drawing.Point(197, 678);
             this.player1L.Name = "player1L";
-            this.player1L.Size = new System.Drawing.Size(83, 20);
+            this.player1L.Size = new System.Drawing.Size(75, 17);
             this.player1L.TabIndex = 0;
             this.player1L.Text = "m_Player1";
             // 
             // player2L
             // 
             this.player2L.AutoSize = true;
-            this.player2L.Location = new System.Drawing.Point(502, 600);
+            this.player2L.Location = new System.Drawing.Point(438, 678);
             this.player2L.Name = "player2L";
-            this.player2L.Size = new System.Drawing.Size(83, 20);
+            this.player2L.Size = new System.Drawing.Size(75, 17);
             this.player2L.TabIndex = 1;
             this.player2L.Text = "m_Player2";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(95, 37);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 600);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
             // Board
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 680);
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.player2L);
             this.Controls.Add(this.player1L);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Board";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TicTacToeMisere";
             this.Load += new System.EventHandler(this.Board_Load);
             this.ResumeLayout(false);
@@ -70,5 +82,6 @@ namespace GameUI
 
         private System.Windows.Forms.Label player1L;
         private System.Windows.Forms.Label player2L;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- GameUI/Board.cs
+++ GameUI/Board.cs
@@ -32,13 +32,40 @@ namespace GameUI
 
         private void Board_Load(object sender, EventArgs e)
         {
+        
+           
+            for (int i = 1 ; i <= m_BoardGame.Size ; i++)
+            {
+                for(int j = 1 ; j <= m_BoardGame.Size; j++)
+                {
+                    flowLayoutPanel1.Controls.Add(CreateCells(i, j));
+                    
+                }
+            }
             player1L.Text = m_Player1.Id + ": " + m_Player1.Score;
             player2L.Text = m_Player2.Id + ": " + m_Player2.Score;
         }
 
-        public void CreateCells()
+        public Button CreateCells(int i , int j)
         {
-            
+            Button cell = new Button();
+            cell.Name = i.ToString() + j.ToString();
+            cell.Width = ((flowLayoutPanel1.Width-50) / (m_BoardGame.Size));
+            cell.Height = ((flowLayoutPanel1.Height-50) / (m_BoardGame.Size));
+            //cell.Width = 50;
+            //cell.Height = 50;
+            //cell.Top = j * flowLayoutPanel1.Height;
+            //cell.Left = i * flowLayoutPanel1.Width;
+            //cell.Height = 90;
+            cell.Text = "";
+            //cell.Click += cell_click;
+            return cell;
         }
+
+        //public void cell_click(object sender, EventArgs e)
+        //{
+        //    Button cell = (Button)sender;
+        //    //MessageBox.Show(cell.Name.ToString());
+        //}
     }
 }
